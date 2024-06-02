73a07984c1aa8b089de8c424c905c2820d0f09f6
Marcelo Frohlich <96299094+MarceloFrohlich@users.noreply.github.com>
2024-06-02 11:29:32 -0300

    Add files via upload

---
+++ src/app/app/components/trajetoria.tsx
@@ -0,0 +1,36 @@
+const Trajetoria: React.FC = () => {
+    return (
+        <section className="bg-slate-200 flex flex-col items-center w-full" data-aos="fade-up">
+            <h1 className="text-4xl font-bold pt-10 text-lime-700">Minha Trajetória</h1>
+
+            <div className="flex flex-col gap-6 mx-3 md:w-2/3 text-justify py-10">
+                <div className="text-lg">
+                    <p>Iniciei minha carreira no marketing digital como social media em 2016.
+                        Na época, eu nem sabia que o que fazia poderia se tornar uma carreira, muito menos o
+                        nome dessa profissão. Desde então, passei por muitos anos de formação e adquiri
+                        experiências práticas em Gerenciamento de Redes Sociais, principalmente com infoprodutores.
+                    </p>
+                </div>
+
+                <div>
+                    <p>Com o tempo, percebi que algo estava faltando. Não bastava apenas pensar
+                        no conteúdo e esperar pacientemente pelos resultados do tráfego orgânico.
+                        Eu queria ver meus clientes fechando negócios e precisava desses resultados na mesma
+                        velocidade que eles. Foi então que decidi aprender a atrair clientes através de anúncios
+                        online.
+                    </p>
+                </div>
+
+                <div>
+                    <p>Hoje, meu principal foco é a gestão de tráfego pago, ajudando negócios locais crescerem
+                        em resultados através dos anúncios online. Contudo, continuo oferecendo uma gama completa
+                        de serviços em marketing digital, incluindo estratégias de conteúdo, para atender às
+                        diversas necessidades dos meus clientes.
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Trajetoria
